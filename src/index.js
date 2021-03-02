aeac87c7f4e033b8bb85e891629031cec4dd42ee
emkorolev12@gmail.com <emkorolev12@gmail.com>
2021-03-02 18:07:33 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+  console.log(array); 
+  if(array == null || array.length == 0){
+    return 0;
+  }
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+    return 0;
+  }
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+    return 0;
+  }
+  return array.reduce((sum, current) => sum + current,0) / array.length;
 }
